9f474413b61014db90cb18113f77211394a1b2b0
koki <kokides0428@gmail.com>
2023-05-05 14:54:42 +0900

    BatchTaskletConfigTestリファクタリング

--- src/test/java/com/example/batchstudy/config/BatchTaskletConfigTest.java
+++ src/test/java/com/example/batchstudy/config/BatchTaskletConfigTest.java
@@ -4,18 +4,15 @@ import com.github.tomakehurst.wiremock.WireMockServer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-@RunWith(SpringRunner.class)
 @SpringBatchTest
 @SpringBootTest(classes = {BatchTaskletConfig.class})
 class BatchTaskletConfigTest {
