b16d94852858161b5ee71d0eaae55e41ac1b4399
koki <kokides0428@gmail.com>
2023-06-11 14:06:59 +0900

    不要なimportの削除

--- src/test/java/com/example/batchstudy/service/DownloadServiceTest.java
+++ src/test/java/com/example/batchstudy/service/DownloadServiceTest.java
@@ -4,7 +4,6 @@ import com.github.tomakehurst.wiremock.WireMockServer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
